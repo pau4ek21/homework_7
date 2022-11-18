502c12319c94a95d562b97f3979dc9986c55aad1
IgorShkatulo <shkatulo59@gmail.com>
2022-11-18 18:35:20 +0500

    full homework

---
+++ button_click.py
@@ -0,0 +1,16 @@
+import export_data as exp
+import import_data as imp
+import wiew
+
+def button_click():
+    wiew.hello()
+    choise = wiew.button()
+    while choise != 'q':
+        if choise == '1':
+            exp.export_data()
+        elif choise == '2':
+            what_find = input('Для поиска по фамилии введите "1"\nДля поиска по имени введите "2"\nДля поиска по номеру телефона введите "3"\n')
+            if what_find == '1': imp.find_surname(input('Введите фамилию: '))
+            if what_find == '2': imp.find_name(input('Введите имя: '))
+            if what_find == '3': imp.find_phone(input('Введите телефон: '))
+        choise = wiew.button()
---
+++ wiew.py
@@ -0,0 +1,9 @@
+def answer(data):
+    print(data)
+
+def button():
+    inp = input('Для внесения данных нажмите "1"\nДля поиска данных нажмите "2"\nДля выхода нажмите "q"\n')
+    return inp
+
+def hello():
+    print('Программа для работы с базой данных')
